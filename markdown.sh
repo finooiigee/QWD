c6a52bd66f37618d6100d8973c52423245cafd52
finooiigee <essie@lovett.gay>
2023-04-16 17:43:26 +0100

    add assets, change /wiki/index.html

---
+++ markdown.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for i in *.md
+do echo "
+<html>
+<head>
+<link rel=\"stylesheet\" href=\"/public/assets/style.css\" type=\"text/css\">
+<link rel=\"stylesheet\" href=               \"style.css\" type=\"text/css\">
+</head>
+<body>$(markdown $i)</body>
+</html>
+" > "${i%md}html"
+done
